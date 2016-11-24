3064cbb7be3598bf100221080c0acafaf7326317
koteek <yoyoyo@skbkontur.ru>
2016-11-24 19:38:58 +0300

    Removed grandpa analyzer. Use Roslyn instead.

--- RemoveUnusedUsings/Program.cs
+++ RemoveUnusedUsings/Program.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace RemoveUnusedUsings
 {
-    static class Program
+    internal static class Program
     {
-        [STAThread]
-        static void Main(string[] args)
-        {
-            const string solutionKey = "solution";
-            const string verboseKey = "verbose";
+        private const string solutionKey = "solution";
+        private const string verboseKey = "verbose";
 
+        [STAThread]
+        private static void Main(string[] args)
+        {
             var keyValues = args.Select(x => x.Split('=')).ToArray();
             if (args.Length == 0 || keyValues.Any(x => x.Length != 2))
             {
@@ -19,36 +20,47 @@ namespace RemoveUnusedUsings
                 return;
             }
 
-
-            Dictionary<string, string> dictionary;
-            try
-            {
-                dictionary = keyValues.ToDictionary(x => x[0], x => x[1]);
-            }
-            catch (ArgumentNullException)
-            {
-                Console.WriteLine("Please set key before equals sign");
-                return;
-            }
-            catch (ArgumentException)
-            {
-                Console.WriteLine("Please remove key duplicates");
-                return;
-            }
-
-            if (!dictionary.ContainsKey(solutionKey))
-            {
-                Console.WriteLine($"Args must contains '{solutionKey}' key");
-                return;
-            }
+            var dictionary = BuildParametersDictionary(keyValues);
 
             var solutionPath = dictionary[solutionKey];
             var logLevel = dictionary.ContainsKey(verboseKey)
                 ? (dictionary[verboseKey] == "quiet" ? LogLevel.Quiet : LogLevel.Detail)
                 : LogLevel.Detail;
 
-            var organizer = new Organizer(new Logger(logLevel));
+            ILogger logger = new Logger(logLevel);
+            IOrganizer organizer = new RoslynOrganizer(logger);
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
             organizer.OrganizeSolution(solutionPath);
+            stopwatch.Stop();
+            logger.Log($"Elasped: {stopwatch.ElapsedMilliseconds} ms");
+            Console.ReadKey();
+        }
+
+        private static Dictionary<string, string> BuildParametersDictionary(string[][] keyValues)
+        {
+            try
+            {
+                var dictionary =  keyValues.ToDictionary(x => x[0], x => x[1]);
+                if (dictionary.ContainsKey(solutionKey))
+                {
+                    return dictionary;
+                }
+
+                Console.WriteLine($"Args must contains '{solutionKey}' key");
+                return null;
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Please set key before equals sign");
+                return null;
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please remove key duplicates");
+                return null;
+            }
         }
     }
 }
---
+++ RemoveUnusedUsings/RoslynOrganizer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.MSBuild;
+
+namespace RemoveUnusedUsings
+{
+    public class RoslynOrganizer : IOrganizer
+    {
+        private readonly ILogger logger;
+
+        public RoslynOrganizer(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public void OrganizeSolution(string solutionPath)
+        {
+            var workspace = MSBuildWorkspace.Create();
+
+            var originalSolution = workspace.OpenSolutionAsync(solutionPath).Result;
+            var modifiedSolution = originalSolution;
+
+            foreach (var projectId in modifiedSolution.ProjectIds)
+            {
+                var project = modifiedSolution.GetProject(projectId);
+                foreach (var documentId in project.DocumentIds)
+                {
+                    while (true)
+                    {
+                        var document = modifiedSolution.GetDocument(documentId);
+
+                        var root = document.GetSyntaxRootAsync().Result;
+                        var usings = root.DescendantNodes().OfType<UsingDirectiveSyntax>().ToArray();
+
+                        var semanticModel = document.GetSemanticModelAsync().Result;
+                        var diagnostics = semanticModel.GetDiagnostics();
+                        var unusedUsingsCount = diagnostics.Count(IsUnusedUsing);
+                        if (unusedUsingsCount > 0)
+                        {
+                            logger.Log($"Document: {document.FilePath}. Usings count: {unusedUsingsCount}");
+                        }
+                        var unusedUsingError = diagnostics.FirstOrDefault(IsUnusedUsing);
+                        if (unusedUsingError == null)
+                        {
+                            break;
+                        }
+
+                        var @using = usings.FirstOrDefault(u => u.Span == unusedUsingError.Location.SourceSpan);
+
+                        modifiedSolution = document.WithSyntaxRoot(root.RemoveNode(@using, SyntaxRemoveOptions.KeepDirectives)).Project.Solution;
+                    }
+                }
+            }
+
+            workspace.TryApplyChanges(modifiedSolution);
+        }
+
+        private static bool IsUnusedUsing(Diagnostic d)
+        {
+            return string.Equals(d.Id, "CS8019", StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
